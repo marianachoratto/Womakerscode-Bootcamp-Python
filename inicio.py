d29d274c3cf067fab8b45a20b6353f141c277bbe
Mariana Choratto <146736051+marianachoratto@users.noreply.github.com>
2024-01-16 19:12:44 -0300

    primeira aula de react

---
+++ inicio.py
@@ -0,0 +1,60 @@
+print('olá mundo')
+# olá 
+''' Comentário longo'''
+
+# Tipo INT 
+# OBS: no python nao tem variável é só escrever o nome 
+# numero = int(11.5)
+# print(numero)
+
+# bolinha = int(input('Digite seu numero:'))
+# print(bolinha)
+
+#Tipo FLOAT
+'''numero = float(input('Digite um numero float'))
+print(numero)'''
+
+#Tipo STRING
+'''frase = input('Digite sua frase:')
+print(frase)'''
+
+#Operações matemáticas 
+# Soma +
+'''numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite um numero:'))
+
+print(f' A soma dos 2 números é: {numero1 + numero2}')'''
+
+# Subtração 
+'''numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite um numero:'))
+
+print(numero1 - numero2)'''
+
+# Multiplicação 
+'''numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite um numero:'))
+
+print(numero1 * numero2)'''
+
+# Divisão
+'''numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite um numero:'))
+
+print(numero1 // numero2)'''
+
+# Operador de incremento +=/decremento -= 
+'''valor = 5 
+valor -= 1 
+print(valor)'''
+
+# Resto %
+# print(10%3)
+
+# Format
+valor = 55.56685452
+print(f'{valor:.6f}')
+
+print('Olá, {}, você tem {} anos'.format('Mariana', 26))
+
+print('1'+2)
