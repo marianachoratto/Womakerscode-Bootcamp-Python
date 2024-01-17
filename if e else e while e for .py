d790ed13ae797cf53685db3916d2d280114ae036
Mariana Choratto <146736051+marianachoratto@users.noreply.github.com>
2024-01-17 15:22:21 -0300

    aula if e else

---
+++ if e else e while e for .py
@@ -0,0 +1,20 @@
+#IF e ELSE
+'''numero = int(input('Digite um numero'))
+
+if numero > 0:
+    print('esse numero é positivo')
+else :
+    print('esse numero é negativo')'''
+
+#WHILE
+'''numero = -1 
+while numero < 0: 
+    numero = int(input('Digite um número novamente:'))'''
+
+#FOR
+frutas= ['maçã', 'banana', 'laranja', 'uva']
+
+frutas.append('carambola')
+
+for item in frutas:
+    print(item)
