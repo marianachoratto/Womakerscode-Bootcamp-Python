1c0641729e6224c66fb8d7c27ec44e8ce81a1aa6
marianachoratto <marianachoratto@gmail.com>
2024-01-24 19:36:13 -0300

    aula orintação objetos

--- Exercicios/1 entrega modulo 1.py
+++ Exercicios/1 entrega modulo 1.py
@@ -141,7 +141,7 @@ digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom
 Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 '''
 
-# resposta = input('Em que turno você estuda? \n Digite M para Matutino. \n Digite V para vespertino. \n Ou N para noturno.').upper()
+resposta = input('Em que turno você estuda? \n Digite M para Matutino. \n Digite V para vespertino. \n Ou N para noturno.').upper()
 
 
 # if resposta == "M": 
--- orientacao_a_objetos/exercicio_orientacao_obj.py
+++ orientacao_a_objetos/exercicio_orientacao_obj.py
@@ -2,12 +2,41 @@
 # Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
 # Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
 
+class Carro:
+    def __init__(self):
+        self.ligado = False
+        self.cor = 'amarelo'
+        self.velocidade = 0
+    
+    def ligar_carro(self):
+        self.ligado = True
 
+    def desligar_carro(self):
+        self.ligado = False
+    
+    def acelerar_carro(self):
+        self.velocidade += 10
+    
+    def desacelerar_carro(self):
+        self.velocidade -= 10
+    
+    def parar_carro(self):
+        self.velocidade = 0 
 
 # Crie uma instância da classe carro.
-
+fiat0303 = Carro()
 
 # Faça o carro "andar" utilizando os métodos da sua classe.
+fiat0303.acelerar_carro()
+print(f'A velocidade do carro é de {fiat0303.velocidade} km/h')
+
+#Se você quer acelerar o carro 5x você tem que fazer um looping 
+for _ in range(5):
+    fiat0303.acelerar_carro()
+print(f'A velocidade do carro é de {fiat0303.velocidade} km/h')
+    # A função range() serve para catalogar numeros num looping
 
 
 # Faça o carro "parar" utilizando os métodos da sua classe.
+fiat0303.parar_carro()
+print(f'O carro parou. A velocidade chegou a {fiat0303.velocidade} km/h')
