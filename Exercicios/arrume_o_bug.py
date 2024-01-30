1e2cf48e6c164c57decb982e7b2fee70c88b2f2c
marianachoratto <marianachoratto@gmail.com>
2024-01-30 14:34:56 -0300

    exercicio de bug

--- Exercicios/arrume_o_bug.py
+++ Exercicios/arrume_o_bug.py
@@ -2,19 +2,31 @@
 ## No entanto, ele não está funcionando bem. Você pode consertá-lo?
 
 def calcular_media(valores):
-    tamanho = 1
+    tamanho = 0
     soma = 0.0
-    for i, valor in enumerate(valores):
-        soma += valor
-        i += 1
+    for i in valores:
+        soma += i
+        tamanho += 1
+    print(tamanho, soma)
     media = soma / tamanho
+    return media
 
 continuar = True
 valores = []
 while continuar:
     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
-    if valor.lower() == 'ok':
-        continuar = false
+    try:
+        if valor.lower() == 'ok':
+            continuar = False
+        else:
+            valor = float(valor)
+            valores.append(valor)
+    except ValueError as e:
+        print(f'Sintaxe inválida.  {e}. Tente novamente')
+    
 
 media = calcular_media(valores)
 print('A média calculada para os valores {} foi de {}'.format(valores, media))
+
+# Como comparar valores em python?
+# if type(valor) is int:
